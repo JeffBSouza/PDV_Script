6ad8e448c5907ccb2f520ce8aa80ce1c93333221
JeffBSouza <jeh.jorge38@gmail.com>
2025-10-10 13:30:42 -0300

    UpdateFiles

--- GeradorLinkISL.bat
+++ GeradorLinkISL.bat
@@ -13,14 +13,14 @@ set /p "nome=Por favor, informe o nome: "
 
 if "!nome!"=="" (
     echo.
-    echo Erro: Nome nao pode estar vazio!
+    echo Erro: Nome n‹o pode estar vazio!
     timeout /t 2 /nobreak >nul
     goto inicio
 )
 
-set "url_base=https://account.islonline.net/start/ISLAlwaysOn?cmdline=%22password_md5%22+%22c5d7c5561d84f6a20e59bcdccb9093be%22+%22%2fVERYSILENT%22+%22grant_silent%22+%22zeJxtUj1PxDAMVUBsTEyMHQFB1KRN2o6MSAgkkJhucRKnhMslkDR3fCz8dAoj18XD8%2fN7frJHbQ%2fJzeNtdR%2b8C1jd4bSLaX1CRmcOSdvWWmsmukE0rNfIgYNmPVPCYjtgJ1DUQjFpVVdziUbY2jQSawNgByV6NIBKQ4%2bc8VrqDsC0pm8GJmXPRSeElkywBlXNh4YZO8gBZl5rms42vG%2b%2bSSYE%2fA4%2bcgxXF9RlH%2f%2f2pAGnU1IIYdVV9fQwlwfUuLkJeQIPJuYZuR4LJAPBxApeSp6wOlutcrTTDhI%2bx5JxdXFZ%2fYcseveKNMEm5v3uCCo59B6pggTeuAXKbAXbSC0kBwsSzxiSeytIJ3Tv6BLsU17QWkxzZKrKJ0xxn7GexQPQVyh%2bYX4NHwUCjd5tlw02kDT6Bd3fRsk0pgBjWQiXMMzC2fktnH%2bRg%2fL3JB3vmajlfMH2mByRH5IBzI4%3d%22+%22ignore_system_account%22+%22grant_password_md5%22+%22c5d7c5561d84f6a20e59bcdccb9093be%22+%22description%22+%22"
+set "url_base=https://account.islonline.net/start/ISLAlwaysOn?cmdline=%%22password_md5%%22+%%22c5d7c5561d84f6a20e59bcdccb9093be%%22+%%22%%2fVERYSILENT%%22+%%22grant_silent%%22+%%22zeJxtUj1PxDAMVUBsTEyMHQFB1KRN2o6MSAgkkJhucRKnhMslkDR3fCz8dAoj18XD8%%2fN7frJHbQ%%2fJzeNtdR%%2b8C1jd4bSLaX1CRmcOSdvWWmsmukE0rNfIgYNmPVPCYjtgJ1DUQjFpVVdziUbY2jQSawNgByV6NIBKQ4%%2bc8VrqDsC0pm8GJmXPRSeElkywBlXNh4YZO8gBZl5rms42vG%%2b%%2bSSYE%%2fA4%%2bcgxXF9RlH%%2f%%2f2pAGnU1IIYdVV9fQwlwfUuLkJeQIPJuYZuR4LJAPBxApeSp6wOlutcrTTDhI%%2bx5JxdXFZ%%2fYcseveKNMEm5v3uCCo59B6pggTeuAXKbAXbSC0kBwsSzxiSeytIJ3Tv6BLsU17QWkxzZKrKJ0xxn7GexQPQVyh%%2bYX4NHwUCjd5tlw02kDT6Bd3fRsk0pgBjWQiXMMzC2fktnH%%2bRg%%2fL3JB3vmajlfMH2mByRH5IBzI4%%3d%%22+%%22ignore_system_account%%22+%%22grant_password_md5%%22+%%22c5d7c5561d84f6a20e59bcdccb9093be%%22+%%22description%%22+%%22"
 
-set "url_final=!url_base!!nome!&__ISL+Network+Start__hide_gui=1"
+set "url_final=!url_base!!nome!%%22^&__ISL+Network+Start__hide_gui=1"
 
 echo.
 echo Link gerado com sucesso!
@@ -36,7 +36,8 @@ notepad "!temp_file!"
 echo.
 echo O arquivo foi aberto no Notepad.
 echo Pressione qualquer tecla para sair...
-pause >nul
+timeout 2
+exit
 
 del "!temp_file!" >nul 2>&1
 endlocal
